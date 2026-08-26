e67d88f4590b27049bcc68d1d277ca6773eca42b
kalpabanuja <wickramasinghekalpa@gmail.com>
2026-08-27 04:05:36 +0530

    order changed

--- ThreatIntelClient/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ThreatIntelClient;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
