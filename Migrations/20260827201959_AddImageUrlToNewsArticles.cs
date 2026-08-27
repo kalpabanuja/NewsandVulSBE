0f4a1861e8856ba6c27b09de71f89cebff83e24c
kalpabanuja <wickramasinghekalpa@gmail.com>
2026-08-28 01:50:22 +0530

    feat: add ImageUrl field to news articles via database migration

---
+++ Migrations/20260827201959_AddImageUrlToNewsArticles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsandVulSBE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToNewsArticles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "NewsArticles",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "NewsArticles");
+        }
+    }
+}
